12191642a65de3d7bd8559b97dd48df187504d53
Jiyuan Yang <yjy070509@gmail.com>
2026-03-31 20:49:53 +0300

    z

---
+++ app/src/main/java/com/example/space_colony/model/MissionResult.java
@@ -0,0 +1,43 @@
+package com.example.space_colony.model;
+
+import java.util.List;
+
+public class MissionResult {
+    private boolean success;
+    private int xpGained;
+    private int fragmentsGained;
+    private List<CrewMember> crewToMedbay;
+    private String summary;
+
+    public MissionResult(int xpGained, int fragmentsGained, String summary){
+        this.success = success;
+        this.xpGained = xpGained;
+        this.fragmentsGained = fragmentsGained;
+        this.crewToMedbay = crewToMedbay;
+        this.summary = summary;
+    }
+
+    public boolean isSuccess(){
+        return success;
+    }
+
+    public int getXpGained(){
+        return xpGained;
+    }
+
+    public int getFragmentsGained(){
+        return fragmentsGained;
+    }
+
+    public List<CrewMember> getCrewToMedbay(){
+        return crewToMedbay;
+    }
+    public String getSummary(){
+        return summary;
+    }
+
+    public void addCrewToMedbay(CrewMember crewMember){
+        crewToMedbay.add(crewMember);
+    }
+
+}
