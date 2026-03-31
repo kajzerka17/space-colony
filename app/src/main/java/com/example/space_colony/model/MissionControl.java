a28519aa9ab00a6c602ff5cd94d0b803459bba05
gracjansiemczonek <gracjansiemwp.pl@gmail.com>
2026-03-31 20:11:26 +0300

    added misson system basic

---
+++ app/src/main/java/com/example/space_colony/model/MissionControl.java
@@ -0,0 +1,21 @@
+package com.example.space_colony.model;
+
+import java.util.Random;
+
+public class MissionControl {
+    protected Mission currentMission;
+    protected list<CrewMember>selectedCrew;
+    public Mission generateMission(int day) {
+        Random rand = new Random();
+        int roll = rand.nextInt(100);
+        // Enemy attack chance increases with day, capped at 60%
+        int enemyChance = Math.min(5 + day * 5, 60);
+        int type;
+        if (roll < enemyChance) {
+            type = 2; // enemy attack
+        } else {
+            type = rand.nextInt(2); // 0 or 1 for other mission types
+        }
+        return new Mission(type);
+    }
+}
