ba3a074c558cae8e5b918430f49b9ebe58b34cde
An Hoang <an9201nbk@gmail.com>
2026-04-09 19:06:48 +0300

    recyclerview

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,7 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(libs.recyclerview)
+    implementation(libs.cardview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
