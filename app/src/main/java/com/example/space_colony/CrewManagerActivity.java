1b70edabd164d6b79ca84900db78775ef1268f48
An Hoang <an9201nbk@gmail.com>
2026-04-09 18:30:34 +0300

    smthing

--- app/src/main/java/com/example/space_colony/CrewManagerActivity.java
+++ app/src/main/java/com/example/space_colony/CrewManagerActivity.java
@@ -1,8 +1,6 @@
 package com.example.space_colony;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainGame extends AppCompatActivity {
+public class CrewManagerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main_game);
+        setContentView(R.layout.activity_crew_manager);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        Button startButton = findViewById(R.id.crewManagerButton);
-        startButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainGame.this, CrewManagerActivity.class);
-            startActivity(intent);
         });
     }
 }
