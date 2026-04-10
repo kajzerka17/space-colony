0cc94164c9b88066478bb09d436f7b947a8b86cc
An Hoang <an9201nbk@gmail.com>
2026-04-10 16:24:29 +0300

    recycler view for crews

--- app/src/main/java/com/example/space_colony/CrewManagerActivity.java
+++ app/src/main/java/com/example/space_colony/CrewManagerActivity.java
@@ -1,15 +1,34 @@
 package com.example.space_colony;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.space_colony.adapter.CrewMemberAdapter;
+import com.example.space_colony.model.Blacksmith;
+import com.example.space_colony.model.CrewMember;
+import com.example.space_colony.model.GameManager;
+import com.example.space_colony.model.Medic;
+import com.example.space_colony.model.Scientist;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CrewManagerActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private CrewMemberAdapter adapter;
+//    GameManager manager = GameManager.getInstance();
+//    private List<CrewMember> crews = manager.getQuarters().getCrew();
 
+    private List<CrewMember> crews = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +39,28 @@ public class CrewManagerActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+
+        Button backButton = findViewById(R.id.backButton);
+        Button recruitButton = findViewById(R.id.recruitButton);
+        recyclerView = findViewById(R.id.recyclerViewCrew);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        //crews = new ArrayList<>(); // or fetch from your data source
+        crews.add(new Medic("An"));
+        crews.add(new Blacksmith("Gracjian"));
+        crews.add(new Scientist("jiyuan"));
+        adapter = new CrewMemberAdapter(crews);
+        recyclerView.setAdapter(adapter);
+
+        backButton.setOnClickListener(v -> finish());
+//        btnCreate.setOnClickListener(v -> {
+//            startActivity(new Intent(this, CreateCrewMemberActivity.class));
+//        });
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/space_colony/adapter/CrewMemberAdapter.java
+++ app/src/main/java/com/example/space_colony/adapter/CrewMemberAdapter.java
@@ -1,33 +1,65 @@
 package com.example.space_colony.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.space_colony.R;
 import com.example.space_colony.model.CrewMember;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CrewMemberAdapter<T extends CrewMember> extends RecyclerView.Adapter<CrewMemberAdapter.ViewHolder>{
+
+    private List<T> items;
+
+    public CrewMemberAdapter(List<T> items) {
+        this.items         = new ArrayList<>(items);
+        //this.clickListener = clickListener;
+    }
+
+    public void updateData(List<T> newItems) {
+        this.items = new ArrayList<>(newItems);
+        notifyDataSetChanged();
+    }
+
     @NonNull
     @Override
     public CrewMemberAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_crewmember_recyclerview, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CrewMemberAdapter.ViewHolder holder, int position) {
-
+        T item = items.get(position);
+        holder.tvName.setText(item.getName());
+        holder.tvRole.setText(item.getSpecialization());
+        // Lambda click handler
+//        holder.itemView.setOnClickListener(v -> {
+//            if (clickListener != null) clickListener.onItemClick(item);
+//        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return items.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView tvName;
+        final TextView tvRole;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvRole = itemView.findViewById(R.id.tvRole);
         }
     }
 }
