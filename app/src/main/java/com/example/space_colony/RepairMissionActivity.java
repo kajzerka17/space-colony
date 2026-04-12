7b15499c29c5540f2325329b9a932f4073d893dc
gracjansiemczonek <gracjansiemwp.pl@gmail.com>
2026-04-12 22:18:29 +0300

    updated resource, repair, fight mission activity

--- app/src/main/java/com/example/space_colony/RepairMissionActivity.java
+++ app/src/main/java/com/example/space_colony/RepairMissionActivity.java
@@ -8,13 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class TrainCrewActivity extends AppCompatActivity {
+public class RepairMissionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_train_crew);
+        setContentView(R.layout.activity_repair_mission);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
