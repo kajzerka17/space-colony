cf2f279accab59d602fa4a9d65490af18e3af959
Jiyuan Yang <yjy070509@gmail.com>
2026-04-16 22:16:51 +0300

    Clean and comment Engineer

--- app/src/main/java/com/example/space_colony/model/Engineer.java
+++ app/src/main/java/com/example/space_colony/model/Engineer.java
@@ -2,13 +2,18 @@ package com.example.space_colony.model;
 
 import java.util.List;
 
+// engineer crew
 public class Engineer extends SupportClass {
+
+    // make engineer
     public Engineer(String name) {
         super(name);
         this.specialization = "Engineer";
         this.maxEnergy = 20;
         this.energy = 20;
     }
+
+    // no team bonus
     @Override
     public void applyPassiveBonus(List<Fighter> team) {
     }
