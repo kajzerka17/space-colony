5eeb4242af6e7c806708d46475c241f7c3e8658e
Jiyuan Yang <yjy070509@gmail.com>
2026-04-16 22:48:06 +0300

    Clean and comment ResourceMissionActivity

--- app/src/main/java/com/example/space_colony/ResourceMissionActivity.java
+++ app/src/main/java/com/example/space_colony/ResourceMissionActivity.java
@@ -1,33 +1,23 @@
 package com.example.space_colony;
 
 import android.app.Dialog;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import com.example.space_colony.dialog.CrewSelectionDialog;
-import com.example.space_colony.model.CrewMember;
-import com.example.space_colony.model.GameManager;
-import com.example.space_colony.model.MissionControl;
 import com.example.space_colony.model.MissionResult;
 
+// resource mission screen
 public class ResourceMissionActivity extends MissionActivity {
+
+    private Dialog resultDialog;
 
     @Override
     protected int getLayout() {
         return R.layout.activity_resource_mission;
     }
 
-    private Dialog resultDialog;
+    // start resource mission
     @Override
     protected void setBeginButton() {
         Button btnBegin = findViewById(R.id.beginButton);
@@ -40,25 +30,27 @@ public class ResourceMissionActivity extends MissionActivity {
             MissionResult result = manager.launchMission();
 
             if (result != null && result.isSuccess()) {
-                Dialog dialog = new Dialog(this);
-                dialog.setContentView(R.layout.resource_mission_end_dialog_layout);
+                resultDialog = new Dialog(this);
+                resultDialog.setContentView(R.layout.resource_mission_end_dialog_layout);
 
-                TextView resultText = dialog.findViewById(R.id.ResourceResultText);
+                TextView resultText = resultDialog.findViewById(R.id.ResourceResultText);
                 resultText.setText("You've gathered " + result.getFragmentsGained() + " fragments!");
 
-                Button btnAccept = dialog.findViewById(R.id.btnAccept);
+                Button btnAccept = resultDialog.findViewById(R.id.btnAccept);
                 btnAccept.setOnClickListener(d -> {
-                    dialog.dismiss();
+                    resultDialog.dismiss();
                     finish();
                 });
 
-                dialog.setCancelable(false); // force them to press OK
-                dialog.show();
+                resultDialog.setCancelable(false);
+                resultDialog.show();
             } else {
                 finish();
             }
         });
     }
+
+    // clear dialog
     @Override
     protected void onDestroy() {
         super.onDestroy();
