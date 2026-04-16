42335252936e33f1ac53a765999f563bf3c4dba3
Jiyuan Yang <yjy070509@gmail.com>
2026-04-16 22:31:07 +0300

    Clean and comment Scientist

--- app/src/main/java/com/example/space_colony/model/Scientist.java
+++ app/src/main/java/com/example/space_colony/model/Scientist.java
@@ -2,20 +2,28 @@ package com.example.space_colony.model;
 
 import java.util.List;
 
+// scientist crew
 public class Scientist extends SupportClass {
     private static final int[] XP_MILESTONES = {3, 5, 10, 20};
     private int bonusMaxEnergy;
+
+    // make scientist
     public Scientist(String name) {
         super(name);
         this.specialization = "Scientist";
         this.maxEnergy = 20;
         this.energy = 20;
     }
+
+    // give team energy bonus
     @Override
     public void applyPassiveBonus(List<Fighter> team) {
-        for (Fighter fighter : team)  fighter.bonusMaxEnergy += this.bonusMaxEnergy;
+        for (Fighter fighter : team) {
+            fighter.bonusMaxEnergy += this.bonusMaxEnergy;
+        }
     }
 
+    // add xp bonus
     @Override
     public void gainXp(int amount) {
         int xpBefore = this.xp;
