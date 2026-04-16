6797604bff6aec6764715e59c59ef34d30e92cd7
Jiyuan Yang <yjy070509@gmail.com>
2026-04-16 22:22:50 +0300

    Clean and comment Medic

--- app/src/main/java/com/example/space_colony/model/Medic.java
+++ app/src/main/java/com/example/space_colony/model/Medic.java
@@ -1,19 +1,21 @@
 package com.example.space_colony.model;
 
-import javax.xml.transform.sax.TemplatesHandler;
-
+// medic crew
 public class Medic extends Fighter{
     private int healAmount;
+
+    // make medic
     public Medic(String name) {
         super(name);
         this.specialization = "Medic";
         this.maxEnergy = 22;
-        //was 22
         this.energy = 22;
         this.attack = 4;
         this.resilience = 6;
         this.healAmount = 5;
     }
+
+    // heal one crew
     @Override
     public void useSpecialSkill(Threat target, Fighter ally) {
         ally.heal(healAmount);
