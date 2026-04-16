1e12e6b46f7058cfdc53718951e8b869613bb57e
Jiyuan Yang <yjy070509@gmail.com>
2026-04-16 22:34:55 +0300

    Clean and comment SupportClass

--- app/src/main/java/com/example/space_colony/model/SupportClass.java
+++ app/src/main/java/com/example/space_colony/model/SupportClass.java
@@ -2,11 +2,15 @@ package com.example.space_colony.model;
 
 import java.util.List;
 
+// base support class
 public abstract class SupportClass extends CrewMember {
 
+    // make support crew
     public SupportClass(String name) {
         super(name);
     }
+
+    // give team bonus
     public abstract void applyPassiveBonus(List<Fighter> team);
 
 }
