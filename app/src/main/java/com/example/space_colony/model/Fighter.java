0dd3c077cb2242f23bf98ad8c7e0e8ff0cf6fc96
Jiyuan Yang <yjy070509@gmail.com>
2026-04-16 22:17:48 +0300

    Clean and comment Fighter

--- app/src/main/java/com/example/space_colony/model/Fighter.java
+++ app/src/main/java/com/example/space_colony/model/Fighter.java
@@ -2,12 +2,15 @@ package com.example.space_colony.model;
 
 import static java.lang.Math.max;
 
+// base fighter class
 public abstract class Fighter extends CrewMember{
     protected int attack;
     protected int resilience;
     protected int bonusResilience;
     protected int bonusAttack;
     protected int bonusMaxEnergy;
+
+    // make fighter
     public Fighter(String name) {
         super(name);
         this.status = CrewStatus.READY;
@@ -15,13 +18,18 @@ public abstract class Fighter extends CrewMember{
         this.bonusMaxEnergy = 0;
         this.bonusAttack = 0;
     }
+
     public int getEffectiveResilience() { return resilience + bonusResilience; }
     public int getEffectiveAttack() { return attack + bonusAttack; }
     public int getEffectiveMaxEnergy() { return maxEnergy + bonusMaxEnergy; }
+
+    // fill fighter energy
     @Override
     public void restoreEnergy() {
         this.energy = this.getEffectiveMaxEnergy();
     }
+
+    // do attack
     public void performAttack(Threat target) {
         int damage = this.getEffectiveAttack() - target.getResilience();
 
@@ -31,13 +39,19 @@ public abstract class Fighter extends CrewMember{
 
         target.takeDamage(damage);
     }
+
+    // lose energy
     public void takeDamage(int damage) {
         this.energy = max(0,this.energy - damage);
     }
+
+    // heal energy
     public void heal(int amount) {
         this.energy += amount;
         this.energy = max(this.energy,getEffectiveMaxEnergy());
     }
+
+    // use skill
     abstract void useSpecialSkill(Threat target, Fighter ally);
 
 }
