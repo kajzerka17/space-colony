e9cc07fafe5a2530535952d6ccd5076044eca0a1
An Hoang <an9201nbk@gmail.com>
2026-04-17 16:16:50 +0300

    Special skill added. not yet testing

--- app/src/main/java/com/example/space_colony/model/Soldier.java
+++ app/src/main/java/com/example/space_colony/model/Soldier.java
@@ -14,7 +14,7 @@ public class Soldier extends Fighter{
         this.energy = 18;
         this.attack = 7;
         this.resilience = 5;
-        this.powerStrikeUses = 2;
+        //this.powerStrikeUses = 2;
     }
 
     public int getPowerStrikeUses(){
@@ -24,10 +24,20 @@ public class Soldier extends Fighter{
     // use power strike
     @Override
     public void useSpecialSkill(Threat target, List<Fighter> ally){
-        if (powerStrikeUses > 0){
-            powerStrikeUses = powerStrikeUses - 1;
+//        if (powerStrikeUses > 0){
+//            powerStrikeUses = powerStrikeUses - 1;
+//
+//            int damage = (int) (this.getEffectiveAttack()*1.5 - target.getResilience());
+//
+//            if (damage < 0){
+//                damage = 0;
+//            }
+//
+//            target.takeDamage(damage);
+//            target.reduceResilience(2);
+//        }
 
-            int damage = (int) (this.getEffectiveAttack()*1.5 - target.getResilience());
+        int damage = (int) (this.getEffectiveAttack()*1.5 - target.getResilience());
 
             if (damage < 0){
                 damage = 0;
@@ -35,6 +45,5 @@ public class Soldier extends Fighter{
 
             target.takeDamage(damage);
             target.reduceResilience(2);
-        }
     }
 }
